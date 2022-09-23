4bdac5a0300868f3a89a1a9244cf4e563a73eea8
LuisReyes <lfernandoacevedo57@gmail.com>
2022-09-23 07:55:58 -0500

    first commit

---
+++ WPF_APP_CRUD/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WPF_APP_CRUD
+{
+    class Book
+    {
+        public Book(string price, string title)
+        {
+            this.Title = title;
+            this.Price = price;
+        }
+
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("price")]
+        public string Price { get; set; }
+    }
+}
---
+++ WPF_APP_CRUD/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Driver;
+
+namespace WPF_APP_CRUD
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        static MongoClient client = new MongoClient();
+        static IMongoDatabase db = client.GetDatabase("libraryDB");
+        static IMongoCollection<Book> collection = db.GetCollection<Book>("books");
+
+        public void ReadAllDocuments()
+        {
+            List<Book> list = collection.AsQueryable().ToList<Book>();
+            DataTable.ItemsSource = list;
+            Book b = (Book)DataTable.Items.GetItemAt(0);
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
